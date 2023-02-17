443b9ba4879e65d85ee32f479116f327a4867222
Luke Harris <luke+git@lkhrs.com>
2023-02-17 15:45:40 -0600

    W3P3 - add template

---
+++ academy-expert-hosted-api/components/api.js
@@ -0,0 +1,46 @@
+// The API endpoint
+let endpoint = 'https://path-to-your.workers.dev';
+
+/**
+ * Save photos to session storage
+ * @param  {Array} photos The photo data
+ */
+function savePhotos (photos) {
+	sessionStorage.setItem('photos', JSON.stringify(photos));
+}
+
+/**
+ * Get saved photo data from session storage
+ * @return {Array} The photo data
+ */
+function getSavedPhotos () {
+	return JSON.parse(sessionStorage.getItem('photos'));
+}
+
+/**
+ * Fetch photos from the API
+ */
+async function getPhotos () {
+
+	// Check for saved data
+	let saved = getSavedPhotos();
+	if (saved) {
+		return saved;
+	}
+
+	// Otherwise, fetch fresh data from the API
+	try {
+		let response = await fetch(endpoint);
+		if (!response.ok) throw response;
+		let photos = await response.json();
+		savePhotos(photos);
+		return photos;
+	} catch (error) {
+		console.warn(error);
+		return [];
+	}
+
+}
+
+
+export {getPhotos};
