79809561db77eb9d9374d6ab62d82de982177a2b
Luke Harris <luke+git@lkhrs.com>
2023-02-22 14:35:20 -0600

    W4P2

---
+++ academy-expert-database/components/endpoints.js
@@ -0,0 +1,9 @@
+// The photos API endpoint
+let photosURL = 'https://gmt-hosted-api-secure.lw.workers.dev';
+
+// The stripe API endpoint
+let stripeURL = 'https://gmt-stripe-api-secure.lw.workers.dev';
+// let stripeURL = "http://127.0.0.1:8787";
+
+
+export {photosURL, stripeURL};
---
+++ academy-expert-database/gmt-hosted-api-secure/_index.js
@@ -0,0 +1,45 @@
+/**
+ * Respond to the request
+ * @param {Request} request
+ */
+async function handleRequest(request) {
+
+	let headers = new Headers({
+		'Access-Control-Allow-Origin': '*',
+		'Access-Control-Allow-Methods': 'GET, OPTIONS, HEAD',
+		'Access-Control-Allow-Headers': '*'
+	});
+
+	let allowed = ['http://localhost:3000', 'https://lkhrs.github.io']
+
+	// If domain is not allowed, return error code
+	if (!allowed.includes(request.headers.get('origin'))) {
+			return new Response('Not allowed', {
+					status: 403,
+					headers: headers
+			});
+	}
+
+	// If not a GET request, return generic response
+	if (request.method !== 'GET') {
+		return new Response('ok', {
+			status: 200,
+			headers: headers
+		});
+	}
+
+	// Get photos from the database
+	let photos = await PHOTOS.get('photos');
+
+	// Otherwise, return photos
+	return new Response(photos, {
+		status: 200,
+		headers: headers
+	});
+
+}
+
+// Listen for API calls
+addEventListener('fetch', function (event) {
+	event.respondWith(handleRequest(event.request));
+});
