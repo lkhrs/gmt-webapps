be396aa4b09a8166bc89dea9f158b16d1cd95e5b
Luke Harris <luke+git@lkhrs.com>
2023-03-07 14:52:12 -0600

    Add W5P1

---
+++ academy-expert-login-template/components/endpoints.js
@@ -0,0 +1,8 @@
+// The photos API endpoint
+let photosURL = 'https://path-to-your.workers.dev';
+
+// The stripe API endpoint
+let stripeURL = 'https://path-to-stripe.workers.dev';
+
+
+export {photosURL, stripeURL};
