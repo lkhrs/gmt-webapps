586c82d398290bfdcb1b902029338447ea90dc7c
Luke Harris <luke+git@lkhrs.com>
2023-03-07 15:22:29 -0600

    W5P1 completed

--- academy-expert-login-template/components/endpoints.js
+++ academy-expert-login-template/components/endpoints.js
@@ -4,5 +4,8 @@ let photosURL = 'https://path-to-your.workers.dev';
 // The stripe API endpoint
 let stripeURL = 'https://path-to-stripe.workers.dev';
 
+// Auth API endpoint
+let authURL = "https://vanillajsacademy.com/api/login.php";
 
-export {photosURL, stripeURL};
+
+export {photosURL, stripeURL, authURL};
